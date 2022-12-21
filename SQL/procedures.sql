3a5b619ecf40a0abde97d37784aabd21fa222091
Wein <nikita0zrz@gmail.com>
2022-12-21 22:44:25 +0300

    Update procedures.sql

--- SQL/procedures.sql
+++ SQL/procedures.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE create_user(login VARCHAR(128), password_text VARCHAR(256), first_name_text VARCHAR(128), last_name_text VARCHAR(128), country TEXT, city TEXT, image_base64 TEXT)
+CREATE OR REPLACE PROCEDURE create_user(login VARCHAR(128), password_text VARCHAR(256), first_name_text VARCHAR(128), last_name_text VARCHAR(128), country TEXT, city TEXT)
 AS $$
 DECLARE
     img_id INTEGER;
@@ -7,10 +7,10 @@ BEGIN
         RAISE EXCEPTION 'Такой пользователь уже существует';
     ELSE
         INSERT INTO user_login(username, password) VALUES (login, crypt(password_text, gen_salt('md5')));
-        INSERT INTO images(image) VALUES (image_base64) RETURNING id INTO img_id;
         INSERT INTO user_info(user_id, first_name, last_name, country_id, city_id, image_id)
-        VALUES (get_user_id_by_login(login), first_name_text, last_name_text, get_county_id(country), get_city_id(city), img_id);
+        VALUES (get_user_id_by_login(login), first_name_text, last_name_text, get_county_id(country), get_city_id(city), 1);
         COMMIT;
     END IF;
 END
 $$LANGUAGE plpgsql;
+
