a59ae37cd9bba64fa15c0fab1b0fc60895e4b4c5
Nikita228133769 <nikita0zrz@gmail.com>
2022-12-21 23:36:35 +0300

    Update procedures.sql

--- SQL/procedures.sql
+++ SQL/procedures.sql
@@ -1,7 +1,6 @@
 CREATE OR REPLACE PROCEDURE create_user(login VARCHAR(128), password_text VARCHAR(256), first_name_text VARCHAR(128), last_name_text VARCHAR(128), country TEXT, city TEXT)
 AS $$
 DECLARE
-    img_id INTEGER;
 BEGIN
     IF (SELECT COUNT(*) FROM user_login WHERE username = login) THEN
         RAISE EXCEPTION 'Такой пользователь уже существует';
@@ -9,6 +8,9 @@ BEGIN
         INSERT INTO user_login(username, password) VALUES (login, crypt(password_text, gen_salt('md5')));
         INSERT INTO user_info(user_id, first_name, last_name, country_id, city_id, image_id)
         VALUES (get_user_id_by_login(login), first_name_text, last_name_text, get_county_id(country), get_city_id(city), 1);
+        EXECUTE format('CREATE USER %I WITH PASSWORD %L', login, password_text::VARCHAR);
+        EXECUTE format('GRANT user to %I', login);
+        COMMIT;
         COMMIT;
     END IF;
 END
