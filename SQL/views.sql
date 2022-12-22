168b7d05103cb60688685f0fd54570bab17fdaae
Ilya Lugovoy <ilya.l2013@yandex.ru>
2022-12-23 02:54:37 +0300

    Update views.sql

--- SQL/views.sql
+++ SQL/views.sql
@@ -8,7 +8,7 @@ CREATE VIEW get_publishers AS
     SELECT name FROM publisher;
 
 CREATE VIEW get_games AS
-    SELECT g.name AS game, p.name as publisher, g.price, string_agg(gn.name, ', ') AS Genres, i.image FROM game g
+    SELECT g.name AS game, p.name as publisher, g.price, string_agg(gn.name, ', ') AS genres, i.image FROM game g
     JOIN publisher p ON g.publisher_id = p.id
     JOIN game_genre gr ON g.id = gr.game_id
     JOIN genre gn ON gr.genre_id = gn.id
