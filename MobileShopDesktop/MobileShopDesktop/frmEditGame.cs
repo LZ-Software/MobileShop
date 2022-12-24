e12ae36128231d5476edb2b222cce2ee30ec1e63
Nikita228133769 <nikita0zrz@gmail.com>
2022-12-24 04:14:10 +0300

    Update frmEditGame.cs

--- MobileShopDesktop/MobileShopDesktop/frmEditGame.cs
+++ MobileShopDesktop/MobileShopDesktop/frmEditGame.cs
@@ -19,6 +19,7 @@ namespace MobileShopDesktop
     {
         private string base64ImageOriginal;
         private string base64Image;
+        int g_id;
 
         public frmEditGame()
         {
@@ -122,6 +123,7 @@ namespace MobileShopDesktop
                     {
                         while (reader.Read())
                         {
+                            g_id = reader.GetInt32(reader.GetOrdinal("g_id"));
                             nameText.Text = reader.GetString(reader.GetOrdinal("g_name"));
                             publisherComboBox.SelectedItem = reader.GetString(reader.GetOrdinal("p_name"));
                             priceText.Text = reader.GetDouble(reader.GetOrdinal("price")).ToString();
@@ -194,7 +196,7 @@ namespace MobileShopDesktop
 
                 // -------------------------------------------------------
 
-                NpgsqlCommand cmd1 = new NpgsqlCommand("INSERT INTO images(image) VALUES($1) RETURNUNG id", connection, transaction);
+                NpgsqlCommand cmd1 = new NpgsqlCommand("INSERT INTO images(image) VALUES($1) RETURNING id", connection, transaction);
 
                 cmd1.Parameters.AddWithValue(base64Image);
 
@@ -208,14 +210,14 @@ namespace MobileShopDesktop
                         "description = $2," +
                         "price = $3," +
                         "publisher_id = get_publisher_id_by_title($4)," +
-                        "dt_release = $5::DATE," +
+                        "dt_release = $5::timestamp," +
                         "image_id = $6 WHERE id = $7", connection, transaction);
 
                 cmd2.Parameters.AddWithValue(nameText.Text.Trim());
                 cmd2.Parameters.AddWithValue(descriptionText.Text.Trim());
                 cmd2.Parameters.AddWithValue(float.Parse(priceText.Text, CultureInfo.InvariantCulture.NumberFormat));
                 cmd2.Parameters.AddWithValue(publisherComboBox.SelectedItem.ToString());
-                cmd2.Parameters.AddWithValue(dateReleasePicker.SelectedText);
+                cmd2.Parameters.AddWithValue(dateReleasePicker.DateTime);
                 cmd2.Parameters.AddWithValue(image_id);
                 cmd2.Parameters.AddWithValue(g_id);
 
