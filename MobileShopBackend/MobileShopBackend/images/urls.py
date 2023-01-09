6be0b6c247275e4fa93c9844855790594bfd087c
Wein <nikita0zrz@gmail.com>
2023-01-09 23:06:48 +0300

    Update urls.py

--- MobileShopBackend/MobileShopBackend/images/urls.py
+++ MobileShopBackend/MobileShopBackend/images/urls.py
@@ -3,5 +3,5 @@ from django.conf import urls
 from . import views
 
 urlpatterns = [
-    urls.url(r'^create$', views.CreateImage.as_view(), name='set-image'),
+    urls.url(r'^create$', views.CreateImage.as_view(), name='create-image'),
 ]
