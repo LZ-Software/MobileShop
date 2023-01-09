8eaebb788b3b2c324d58e6bfe4702598c957d505
Wein <nikita0zrz@gmail.com>
2023-01-10 00:37:11 +0300

    [Backend] Update urls

--- MobileShopBackend/MobileShopBackend/game/urls.py
+++ MobileShopBackend/MobileShopBackend/game/urls.py
@@ -3,6 +3,6 @@ from django.conf import urls
 from . import views
 
 urlpatterns = [
-    urls.url(r'^game/create$', views.CreateGame.as_view(), name='create-game'),
-    urls.url(r'^game/delete$', views.DeleteGame.as_view(), name='delete-game'),
+    urls.url(r'^create$', views.CreateGame.as_view(), name='create-game'),
+    urls.url(r'^delete$', views.DeleteGame.as_view(), name='delete-game'),
 ]
