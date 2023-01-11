e3bd08443113e11d11ff98fb2492cd8126c9af96
Ilya Lugovoy <ilya.l2013@yandex.ru>
2023-01-11 14:34:11 +0300

    [Backend] Update

--- MobileShopBackend/MobileShopBackend/game/urls.py
+++ MobileShopBackend/MobileShopBackend/game/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     urls.url(r'^delete$', views.DeleteGame.as_view(), name='delete-game'),
     urls.url(r'^edit$', views.EditGame.as_view(), name='edit-game'),
     urls.url(r'^get$', views.GetGame.as_view(), name='get-game'),
-    urls.url(r'^gets$', views.GetGame.as_view(), name='get-games'),
+    urls.url(r'^list$', views.GetGames.as_view(), name='get-games'),
 ]
